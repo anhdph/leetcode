619e33f0b50423919f5f1c2b7f252b574a0068bb
Phuong Anh Duong <anhdph252@gmail.com>
2026-09-14 13:19:45 +0700

    Time: 272 ms (79.36%), Space: 68.3 MB (11.45%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.py
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
     df = customers.merge(
-        orders['customerId'],
+        orders['customerId'].drop_duplicates(),
         left_on='id',
         right_on='customerId',
         how='left'
