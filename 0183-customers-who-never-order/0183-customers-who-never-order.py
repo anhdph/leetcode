0b61faf68cd2bf3f1f88ce707373a9ea617866da
Phuong Anh Duong <anhdph252@gmail.com>
2026-09-14 13:18:05 +0700

    Time: 307 ms (25.59%), Space: 68.2 MB (16.88%) - LeetHub

---
+++ 0183-customers-who-never-order/0183-customers-who-never-order.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    df = customers.merge(
+        orders['customerId'],
+        left_on='id',
+        right_on='customerId',
+        how='left'
+    )
+
+    df.rename(columns={'name': 'Customers'}, inplace=True)
+
+    return df.loc[df['customerId'].isna(), ['Customers']]
