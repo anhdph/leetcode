b2cb772b1d312aa9adafa7e75e9166e091384230
Phuong Anh Duong <anhdph252@gmail.com>
2026-09-15 11:41:47 +0700

    Time: 340 ms (53.96%), Space: 69.1 MB (18.26%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.py
+++ 0262-trips-and-users/0262-trips-and-users.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
-def less_than_one_trip(trips: pd.DataFrame) -> list:
-
-    trips = trips.groupby('request_at').size().reset_index(name='count')
-    return trips['request_at'][trips['count'] < 1]
-
 def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
 
     banned_users = users['users_id'][
@@ -12,19 +7,16 @@ def trips_and_users(trips: pd.DataFrame, users: pd.DataFrame) -> pd.DataFrame:
         (users['role'].isin(['driver', 'client']))
     ]
 
-    ltot = less_than_one_trip(trips)
-
     trips['request_at'] = pd.to_datetime(trips['request_at'])
 
     trips = trips[
         (trips['request_at'].between('2013-10-01', '2013-10-03')) &
-        (~trips['request_at'].isin(ltot)) &
         (~trips['client_id'].isin(banned_users)) &
         (~trips['driver_id'].isin(banned_users))
     ]
     
     cancelled = (
-        trips[['request_at', 'status']].groupby('request_at')
+        trips.groupby('request_at')
         .agg(
             total = ('status', 'size'),
             cancelled = ('status', lambda x: (x != 'completed').sum())
